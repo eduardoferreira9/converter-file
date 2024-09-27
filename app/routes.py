adbe926934751ff093185ec06f8da9fdd1a87b86
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2024-09-27 12:06:50 -0300

    Add: campo para mudar o nome do arquivo

--- app/routes.py
+++ app/routes.py
@@ -11,21 +11,24 @@ def init_routes(app):
     def upload_file():
         if request.method == 'POST':
             file = request.files['file']
+            output_name = request.form['output_name']  # Captura o nome do arquivo convertido
             file_ext = os.path.splitext(file.filename)[1].lower()
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_path)
 
             # Se for JPG, converte para PNG
             if file_ext == '.jpg':
-                output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.png')
+                output_filename = f"{output_name}.png"  # Adiciona a extensão
+                output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
                 convert_jpg_to_png(file_path, output_path)
-                return redirect(url_for('download_file', filename='output.png'))
+                return redirect(url_for('download_file', filename=output_filename))
 
             # Se for DOCX, converte para PDF
             elif file_ext == '.docx':
-                output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.pdf')
+                output_filename = f"{output_name}.pdf"  # Adiciona a extensão
+                output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
                 convert_docx_to_pdf(file_path, output_path)
-                return redirect(url_for('download_file', filename='output.pdf'))
+                return redirect(url_for('download_file', filename=output_filename))
 
         return render_template('upload.html')
 
